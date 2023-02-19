e315e1cf6437bfa994295fdbb2633a4e16fae5bb
khoaphamce <phamkhoaa18@gmail.com>
2023-02-19 15:41:38 +0700

    lab_1: Complete report

--- lab_1/tb.v
+++ lab_1/tb.v
@@ -17,24 +17,44 @@ module bound_flasher_tb;
         $dumpvars(0, bound_flasher_tb);
     end
 
+    always #5 clk = ~clk;
+
     initial begin
-        clk = 0;
-        reset = 1;
+        
     end
 
-    always #10 clk = ~clk;
-
     initial begin
-        #10 
-        reset = 0;
-        #10
+        clk = 0;
+        flick = 0;
+        reset = 1;
+        #100 flick = 1'b1;
+        #50 flick = 1'b0;
+        #450 flick = 1'b1;
+        #400 flick = 1'b0;
+        #50 flick = 1'b1;
+        #250 flick = 1'b0;
+        #100 flick = 1'b1;
+        #100 flick = 1'b0;
+        #100 flick = 1'b1;
+        #100 flick = 1'b0;
+        #200 flick = 1'b1;
+        #100 flick = 1'b0;
+        #50 flick = 1'b1;
+        #100 flick = 1'b0;
+        #300 
         assign flick = 1;
         #10
         repeat(5) @ (clk);
         #10
         assign flick = 0;
         #10
+        repeat(70) @ (clk);
+        #10
+        assign flick = 1;
+        #10
         repeat(90) @ (clk);
+        #10
+        assign flick = 0;
         #10
         assign flick = 0;
         #10
